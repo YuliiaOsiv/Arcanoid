1576f40368abbf71da9637d2a7a1c6ec349ef74a
annatryhub <119021238+annatryhub@users.noreply.github.com>
2023-01-19 10:31:07 +0200

    ball

--- main.py
+++ main.py
@@ -22,4 +22,41 @@ class Paddle:
                 ball.ball_x = -1
 
     def draw(self):
+        self.actor.draw(
+
+class Ball:
+    def __init__(self, speed: int):
+        self.actor = Actor('cookie.png', center=(WIDTH // 2, HEIGHT//2))
+        self.speed = speed
+        self.ball_x = self.speed
+        self.ball_y = self.speed
+        self.radius = 25 #радіус, щоб знайти, чи перетнулась кулька з перешкодою
+
+    def update(self):
+        self.actor.x += self.ball_x
+        self.actor.y += self.ball_y # зміна координат кульки 60 разів на секунду
+
+        if not (0 <= ball.actor.x <= WIDTH): # відбивання від стінок
+            self.ball_x *= -1
+
+        if not (0 <= ball.actor.y <= HEIGHT): # відбивання від верхньої грані
+            self.ball_y *= -1
+
+        if ball.actor.y == HEIGHT: # якщо мʼячик падає, то забирається одне сердечко
+            global hearts_alive
+            global loss
+            hearts_alive.pop(len(hearts_alive)-1) # кількість сердечок зменшується на одне
+            if len(hearts_alive) == 0:
+                loss = True
+            self.actor.y = HEIGHT // 2 # мʼячик падає з середини екрану
+            self.actor.x = WIDTH // 2
+
+    def hits(self):
+        ball.ball_y *= -1 # якщо мʼячик вдаряється, то він відбивається
+        if random.randint(0, 1):
+            ball.ball_x *= 1
+        else:
+            ball.ball_x = -1
+
+    def draw(self):
         self.actor.draw()
